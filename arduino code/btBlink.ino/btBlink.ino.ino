f1605b4cea8b28b43cab8a311415ff34685234b5
Ben Shukman <shukipost@gmail.com>
2015-11-22 19:09:24 -0800

    BL comms working

--- arduino code/btBlink.ino/btBlink.ino.ino
+++ arduino code/btBlink.ino/btBlink.ino.ino
@@ -1,29 +1,29 @@
 String message; //string that stores the incoming message
 
-void setup()
-{
+void setup() {
   Serial1.begin(9600); //set baud rate
   pinMode(13, OUTPUT);    //teensy LED 
 }
 
-void loop()
-{
-  while(Serial.available())
-  {//while there is data available on the serial monitor
-    message+=char(Serial.read());//store string from serial command
-  }
-  if(!Serial.available())
-  {
-    if(message!="")
-    {//if data is available
-      Serial.println(message); //show the data
-      message=""; //clear the data
-    }
-  }
-//  Serial1.println("Hi!  I'm a robot!");
-////  digitalWrite(13, HIGH);
-////  delay(100);
-////  digitalWrite(13, LOW);
-//  delay(1000); //delay
+void loop() {
+//  while(Serial1.available())
+//  {//while there is data available on the serial monitor
+//    Serial1.println("sup");
+//    message+=char(Serial1.read());//store string from serial command
+//  }
+//  if(!Serial1.available())
+//  {
+//    if(message!="")
+//    {//if data is available
+//      Serial1.println("hi");
+//      Serial1.println(message); //show the data
+//      message=""; //clear the data
+//    }
+//  }
+  Serial1.println("hi");
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(1000); //delay
 }
 
--- serialTest.py
+++ serialTest.py
@@ -8,14 +8,14 @@ serial_port = '/dev/cu.HC-05-DevB' #address: 98-D3-31-40-3E-94
 
 s = serial.Serial(serial_port, serial_speed, timeout = 1)
 
-s.write('i want cereal')
-s.read()
+# s.write('i want cereal')
+# s.read()
 
 while True:
 	s.write('i want cereal')
-	time.sleep(0.5)
-	# print('waiting..')
-	s.read()
+	# time.sleep(0.5)
+	print('waiting..')
+	# s.read()
 	message = s.readline()
 	print(message)
 	time.sleep(0.5)
